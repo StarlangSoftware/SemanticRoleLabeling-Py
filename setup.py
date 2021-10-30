f1a0a0c64c96d28ed36255a5103293e290aa7556
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 20:22:37 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-SemanticRoleLabeling',
-    version='1.0.1',
+    version='1.0.2',
     packages=['SemanticRoleLabeling', 'SemanticRoleLabeling.Sentence', 'SemanticRoleLabeling.Sentence.FrameNet',
               'SemanticRoleLabeling.Sentence.Propbank', 'SemanticRoleLabeling.ParseTree',
               'SemanticRoleLabeling.ParseTree.Propbank'],
@@ -11,5 +15,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Semantic Role Labeling Library',
-    install_requires = ['NlpToolkit-AnnotatedSentence', 'NlpToolkit-AnnotatedTree']
+    install_requires = ['NlpToolkit-AnnotatedSentence', 'NlpToolkit-AnnotatedTree'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
